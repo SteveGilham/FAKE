60e49dcca1ecb54e51f009f547d03f226318120e
SteveGilham <SteveGilham@users.noreply.github.com>
2025-10-20 10:24:42 +0100

    More libraries linked

--- src/app/Fake.Net.FTP/FTP.fs
+++ src/app/Fake.Net.FTP/FTP.fs
@@ -33,6 +33,14 @@ module FTP =
             Request: FtpWebRequest
         }
 
+#nowarn 44
+// error FS0044: This construct is deprecated. WebRequest, HttpWebRequest, ServicePoint, and WebClient are obsolete. Use HttpClient instead.
+// HttpClient is not suitable for FTP operations (does not/will not support), so we will continue using FtpWebRequest despite the warning.
+// Grok suggests using FluentFtp instead
+//    let client = new FluentFtp.FtpClient(serverNameIp)
+//    client.Credentials <- System.Net.NetworkCredential(user, password)
+//    client.Connect()
+
     /// <summary>
     /// Gets a connection to the FTP server
     /// </summary>
