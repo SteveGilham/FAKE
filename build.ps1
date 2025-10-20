bd4327e0998b84040de630225f10d96e63a33867
SteveGilham <SteveGilham@users.noreply.github.com>
2025-10-20 17:40:34 +0100

    Force debug for the temporary package

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 dotnet tool uninstall fake-cli
-dotnet pack --version-suffix 1 src/app/fake-cli/fake-cli.fsproj
-dotnet tool install fake-cli --add-source "./src/app/fake-cli/bin/Release" --version 1.0.0-1
+dotnet pack --version-suffix 1 --configuration Debug src/app/fake-cli/fake-cli.fsproj
+dotnet tool install fake-cli --add-source "./src/app/fake-cli/bin/Debuge" --version 1.0.0-1
 dotnet fake -v build --parallel 3 -e FAKE_DETAILED_ERRORS=true
